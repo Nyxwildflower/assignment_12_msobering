17a291f5d876cf6d5920c1a0d3244cc4b453fbf3
Nyxwildflower <130186258+Nyxwildflower@users.noreply.github.com>
2024-08-10 00:51:33 -0500

    Added newest component to app, changed minor styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button
-          backgroundColor={"#000"}
-          onClick={() => {
-            console.log("Button Pressed");
-          }}
-          label="Click Me"
-        />
-      </header>
+      <Button
+        backgroundColor={"#000"}
+        onClick={() => {
+          console.log("Button Pressed");
+        }}
+        label="Click Me"
+      />
+      <Card
+        backgroundColor={"#D3D9D4"}
+        title="test"
+        content="Content with no subtitle"
+      />
     </div>
   );
 }
